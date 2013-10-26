e60f0c52ed42bbc70bcfba58f911a511aa4e9765
Sampson Chen <sampson.chen@gmail.com>
2013-10-26 11:44:22 -0400

    Added commit about crontab to disable_touchpad.sh

--- scripts/disable_touchpad.sh
+++ scripts/disable_touchpad.sh
@@ -2,3 +2,7 @@
 
 # set to 0 to re-enable
 synclient TouchpadOff=1
+
+# tip: open crontab via $ crontab -e and add the following line for running
+# at boot-time
+# @reboot ~/dotfiles/scripts/disable_touchpad.sh
